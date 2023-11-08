98e81b62e507bee69d31481ed34735bf8ca8360d
Oier Cesat <oier.cesat@gmail.com>
2023-11-08 18:19:16 +0100

    Ajout des icones de la page details excursion

---
+++ src/app/components/CarteAvis.tsx
@@ -0,0 +1,88 @@
+import * as React from "react"
+import { StyleProp, TextStyle, View, ViewStyle,Image, ImageStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { colors, spacing, typography } from "app/theme"
+import { Text } from "app/components/Text"
+
+export interface CarteAvisProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+
+  /**
+   * Nombre d'étoile de l'avis
+   */
+  nombreEtoiles: number
+
+  /**
+   * Avis textuel
+   */
+  texteAvis: string
+}
+
+/**
+ * Describe your component here
+ */
+export const CarteAvis = observer(function CarteAvis(props: CarteAvisProps) {
+  const { style } = props
+  const $styles = [$container, style]
+  const { nombreEtoiles, texteAvis } = props;
+
+  var etoilesVides = 5 - nombreEtoiles; 
+
+  return (
+    <View style={$styles}>
+        <View style={$containerEtoiles} >
+      {
+        Array(nombreEtoiles).fill(0).map((item, index) => (
+          <Image
+            style={$icon}
+            key={index}
+            source={require("../../assets/icons/etoile_pleine.png")}
+          >
+          </Image>
+        ))
+      }
+      {
+        Array(etoilesVides).fill(0).map((item, index) => (
+          <Image
+            style={$icon}
+            key={index}
+            source={require("../../assets/icons/etoile_vide.png")}
+          >
+          </Image>
+        ))
+      }
+    </View>
+      <Text style={$text}>{texteAvis}</Text>
+    </View>
+  )
+})
+
+
+const $container: ViewStyle = {
+  justifyContent: "center",
+  borderColor: colors.bordure,
+  borderWidth: 1,
+  borderRadius: spacing.xs,
+  margin: spacing.lg,
+  padding: spacing.lg,
+}
+
+const $text: TextStyle = {
+  fontFamily: typography.primary.normal,
+  color: colors.texte,
+  fontSize: 14,
+}
+
+const $containerEtoiles: ViewStyle = {
+  flexDirection: "row",
+  marginBottom: spacing.xs,
+}
+
+const $icon : ImageStyle = {
+  width: 20,
+  height: 20,
+  tintColor: colors.bouton,
+}
