34bd87b9e9dce0de2534ef79ac839f2f22dcea5f
Oier Cesat <oier.cesat@gmail.com>
2023-11-08 18:18:15 +0100

    Ajout des avis sur l'excursion avec probleme scroll avis

--- src/app/screens/DetailsExcursionScreen.tsx
+++ src/app/screens/DetailsExcursionScreen.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { View, SafeAreaView, ViewStyle, TouchableOpacity, Image, Button, TextStyle } from "react-native";
+import { View, SafeAreaView, ViewStyle, TouchableOpacity, Image, TextStyle, ImageStyle, ScrollView } from "react-native";
 import { AppStackScreenProps } from "app/navigators";
-import { Text, } from "app/components";
+import { Text, CarteAvis } from "app/components";
 import { spacing, colors } from "app/theme";
 import { Dimensions } from "react-native";
 import SwipeUpDown from "react-native-swipe-up-down";
@@ -45,9 +45,9 @@ export const DetailsExcursionScreen: FC<DetailsExcursionScreenProps> = observer(
 );
 
 function itemMini() {
-  return(
-  <View style={$containerPetit}>
-  </View>
+  return (
+    <View style={$containerPetit}>
+    </View>
   )
 }
 
@@ -59,38 +59,69 @@ function itemFull() {
     <View style={$containerGrand} >
       <View style={$containerTitre}>
         <Text text="Col de la marmotte" size="xl" style={$titre} />
-        <TouchableOpacity
-        >
+        <TouchableOpacity>
           <Image
             source={require("../../assets/icons/download.png")}
             style={$iconDownload}
           >
           </Image>
         </TouchableOpacity>
-
       </View>
-      <View>    
+      <View>
         <View style={$containerBouton}>
-          <TouchableOpacity onPress={()=> setIsInfos(true)} >
-            <Text text="Infos" size="lg" style={[$boutonInfosAvis, isInfos? { color: colors.bouton }: {color: colors.text }]} />
+          <TouchableOpacity onPress={() => setIsInfos(true)} >
+            <Text text="Infos" size="lg" style={[$boutonInfosAvis, isInfos ? { color: colors.bouton } : { color: colors.text }]} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=> setIsInfos(false)}>
-            <Text text="Avis" size="lg" style={[$boutonInfosAvis, isInfos? { color: colors.text }: {color: colors.bouton }]} />
+          <TouchableOpacity onPress={() => setIsInfos(false)}>
+            <Text text="Avis" size="lg" style={[$boutonInfosAvis, isInfos ? { color: colors.text } : { color: colors.bouton }]} />
           </TouchableOpacity>
         </View>
-        <View style={[$souligneInfosAvis, isInfos? {left: spacing.xl }: {left: width/2.5 + spacing.xxl } ]}>
+          <View style={[$souligneInfosAvis, isInfos ? { left: spacing.lg } : { left: width - width / 2.5 - spacing.lg / 1.5 }]}>
         </View>
+        {isInfos ? infos() : avis()}
       </View>
+    </View>
+  )
+}
+
+function infos() {
+  return (
+    <>
       <View style={$containerInformations}>
-        <Text text="Description" size="xs" />
-        <Text text="Description" size="xs" />
-        <Text text="Description" size="xs" />
-        <Text text="Description" size="xs" />
+        <View style={$containerUneInformation}>
+          <Image style={$iconInformation}
+            source={require("../../assets/icons/temps.png")}
+          >
+          </Image>
+          <Text text="2" size="xs" />
+        </View>
+        <View style={$containerUneInformation}>
+          <Image style={$iconInformation}
+            source={require("../../assets/icons/explorer.png")}
+          >
+          </Image>
+          <Text text="2" size="xs" />
+        </View>
+        <View style={$containerUneInformation}>
+          <Image style={$iconInformation}
+            source={require("../../assets/icons/difficulteParcours.png")}
+          >
+          </Image>
+          <Text text="2" size="xs" />
+        </View>
+        <View style={$containerUneInformation}>
+          <Image style={$iconInformation}
+            source={require("../../assets/icons/difficulteOrientation.png")}
+          >
+          </Image>
+          <Text text="1" size="xs" />
+        </View>
+
       </View>
       <View style={$containerDescriptionSignalement}>
         <View>
           <Text text="Description" size="xl" />
-          <Text style={$textDescription} text="Pourquoi les marmottes ne jouent-elles jamais aux cartes avec les blaireaux ? Parce qu'elles ont trop peur qu'elles 'marmottent' les règles !" size="xs" />
+          <Text style={$textDescription} text="Pourquoi les marmottes ne jouent-elles jamais aux cartes avec les blaireaux ? Parce qu'elles ont trop peur qu'elles 'marmottent' les règles !" size="xxs" />
         </View>
         <View>
           <Text text="Signalement" size="xl" />
@@ -99,13 +130,24 @@ function itemFull() {
       </View>
       <View style={$containerDenivele}>
         <Text text="Dénivelé" size="xl" />
-        <Image 
+        <Image
           source={require("../../assets/images/denivele.jpeg")}
           style={$imageDenivele}
         >
         </Image>
       </View>
-    </View>
+    </>
+  )
+}
+
+function avis() {
+  return (
+    <ScrollView>
+      <CarteAvis nombreEtoiles={3} texteAvis="Ma randonnée a été gâchée par une marmotte agressive. J'ai dû renoncer à cause de cette petite terreur. Les montagnes ne sont plus ce qu'elles étaient. 😡🏔️" />
+      <CarteAvis nombreEtoiles={3} texteAvis="Ma randonnée a été gâchée par une marmotte agressive. J'ai dû renoncer à cause de cette petite terreur. Les montagnes ne sont plus ce qu'elles étaient. 😡🏔️" />
+      <CarteAvis nombreEtoiles={3} texteAvis="Ma randonnée a été gâchée par une marmotte agressive. J'ai dû renoncer à cause de cette petite terreur. Les montagnes ne sont plus ce qu'elles étaient. 😡🏔️" />
+      <CarteAvis nombreEtoiles={3} texteAvis="Ma randonnée a été gâchée par une marmotte agressive. J'ai dû renoncer à cause de cette petite terreur. Les montagnes ne sont plus ce qu'elles étaient. 😡🏔️" />
+    </ScrollView>
   )
 }
 
@@ -172,41 +214,52 @@ const $containerBouton: ViewStyle = {
   width: width,
 }
 
-const $boutonInfosAvis : TextStyle = {
+const $boutonInfosAvis: TextStyle = {
   borderBottomWidth: 1,
   borderBottomColor: colors.bouton,
 }
 
-const $souligneInfosAvis : ViewStyle = {
+const $souligneInfosAvis: ViewStyle = {
   backgroundColor: colors.bouton,
   height: 2,
-  width: width/2.5,
+  width: width / 2.5,
   position: "relative",
 }
 
 const $containerInformations: ViewStyle = {
   flexDirection: "row",
+  justifyContent: "space-around",
   width: width,
   padding: spacing.lg,
+}
+
+const $containerUneInformation: ViewStyle = {
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-around",
+  width: spacing.xxl,
+}
+
+const $iconInformation: ImageStyle = {
+  width: spacing.lg,
+  height: spacing.lg,
 }
 
 const $containerDescriptionSignalement: ViewStyle = {
   flexDirection: "row",
   justifyContent: "space-around",
-  width: width,
 }
 
-const $textDescription : TextStyle = {
-  width: width/2,
+const $textDescription: TextStyle = {
+  width: width / 2,
 }
 
 const $containerDenivele: ViewStyle = {
-  width: width,
   padding: spacing.lg,
 }
 
 const $imageDenivele: ViewStyle = {
   height: 100,
-  width: width - spacing.xl,
+  width: width - spacing.xxl,
   marginTop: spacing.xs,
 }
