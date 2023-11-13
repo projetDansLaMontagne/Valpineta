1ae183cb2812745fbe5b9f3aed62c5559ac6233d
unknown <alexandre.pascal.ep@gmail.com>
2023-11-13 18:45:44 +0100

    je peux lire le xml mais pas fichier différent (problème syntaxe pas reconnue)

--- src/app/components/GpxDownloader.tsx
+++ src/app/components/GpxDownloader.tsx
@@ -1,54 +1,46 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, Alert } from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import { shareAsync } from 'expo-sharing'
-import { Button } from './Button';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import { parseString } from 'react-native-xml2js'; // Assurez-vous d'utiliser parseString depuis react-native-xml2js
 
+const YourComponent = () => {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const xmlString = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+      <root xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <person>
+          <name>John Doe</name>
+          <age>30</age>
+          <city>New York</city>
+        </person>
+        <person>
+          <name>Jane Smith</name>
+          <age>25</age>
+          <city>Los Angeles</city>
+        </person>
+      </root>`;
 
-export default function GpxDownloader() {
-  
-  const copyFile = async () => {
-    const filename = "test.json";
+    parseString(xmlString, (error, result) => {
+      if (error) {
+        console.error('Error parsing XML:', error);
+      } else {
+        // Les données sont stockées dans le format JSON dans result
+        setData(result);
+      }
+    });
+  }, []);
 
-    try {
-      const fileUri = './test.json'; // Chemin du fichier source
-      const newFileUri = FileSystem.documentDirectory + filename; // Nouvel emplacement du fichier
-  
-      await FileSystem.copyAsync({ from: fileUri, to: newFileUri });
-  
-      console.log('Fichier copié avec succès !');
-    } catch (error) {
-      console.error('Une erreur s\'est produite lors de la copie du fichier : ', error);
-    }
-  };
-
-  const downloadFromUrl = async () => {
-    // const result = await FileSystem.downloadAsync(
-    //   'http://techslides.com/demos/sample-videos/small.mp4',
-    //   FileSystem.documentDirectory + filename
-    // );
-
-    copyFile();
-
-
-
-/*
-      console.log(result);
-    save(result);*/
-
-  };
-
-  const save = (url) => {
-    shareAsync(url);
-  }
-
-  return(
-      
-    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-      <Button text="Telecharge" onPress={downloadFromUrl}/>
-      <StatusBar style="auto" />
+  return (
+    <View>
+      {data && data.root && data.root.person.map((person, index) => (
+        <View key={index}>
+          <Text>Name: {person.name}</Text>
+          <Text>Age: {person.age}</Text>
+          <Text>City: {person.city}</Text>
+        </View>
+      ))}
     </View>
-    )
-}
+  );
+};
+
+export default YourComponent;
