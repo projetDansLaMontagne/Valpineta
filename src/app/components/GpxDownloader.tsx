d7e3bc02ae5cf82ae555ea941a75e70cb9af9c76
Oier Cesat <oier.cesat@gmail.com>
2023-11-17 10:28:23 +0100

    Intégration à la page détail excursion

--- src/app/components/GpxDownloader.tsx
+++ src/app/components/GpxDownloader.tsx
@@ -1,18 +1,49 @@
-import { View,  Button } from 'react-native';
+import * as React from "react"
+import { StyleProp, View, ViewStyle, TouchableOpacity,Image, ImageStyle  } from "react-native"
+import { observer } from "mobx-react-lite"
+import { colors } from "app/theme"
 import * as Sharing from 'expo-sharing';
 import * as FileSystem from 'expo-file-system';
 
-const YourComponent = () => {
+export interface GpxDownloaderProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+}
+
+/**
+ * Describe your component here
+ */
+export const GpxDownloader = observer(function GpxDownloader(props: GpxDownloaderProps) {
+  const { style } = props
+  const $styles = [$container, style]
 
   const downloadAndSaveFile = () => {
     Sharing.shareAsync(FileSystem.documentDirectory + 'PuntaFulsaAciron.gpx');
   };
 
   return (
-    <View>
-      <Button title="Télécharger le fichier" onPress={downloadAndSaveFile} />
+    <View style={$styles}>
+      <TouchableOpacity
+        onPress={() => downloadAndSaveFile()}
+      >
+        <Image
+          source={require("../../assets/icons/download.png")}
+          style={$iconDownload}
+        >
+        </Image>
+      </TouchableOpacity>
     </View>
-  );
-};
+  )
+})
 
-export default YourComponent;
+const $container: ViewStyle = {
+  justifyContent: "center",
+}
+
+const $iconDownload: ImageStyle = {
+  width: 40,
+  height: 40,
+  tintColor: colors.bouton,
+}
