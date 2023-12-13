ef8f765b98c0dcb27d7b7e677a88489671577b9a
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-12-13 12:50:17 +0100

    FIX gestion des GPX ayant plusieurs trkseg + completion des intervalles nulles de debut et de fin

--- api-wp/gpx_to_json.php
+++ api-wp/gpx_to_json.php
@@ -24,11 +24,13 @@ function formatGPXExcursions($gpxFile)
     $excursion = json_encode($excursion);
     $excursion = json_decode($excursion, true);
 
+
     // Verification formatage du GPX
-    if (!isset($excursion["trk"]["trkseg"]["trkpt"])) {
+    if (isset($excursion["trk"][1])) {
+        throw new Exception("GPX doit contenir un seul trkseg");
+    } else if (!isset($excursion["trk"]["trkseg"]["trkpt"])) {
         throw new Exception("GPX Mal formaté");
-    }
-    if (count($excursion["trk"]["trkseg"]["trkpt"]) < 2) {
+    } else if (count($excursion["trk"]["trkseg"]["trkpt"]) < 2) {
         throw new Exception("Nombre de points insuffisants");
     }
 
@@ -58,7 +60,7 @@ function formatGPXExcursions($gpxFile)
     }
 
 
-    
+
     /* ------------------ Completion des zones vides d altitude ----------------- */
     // Recherche des intervalles d altitudes nulles
     $i = 0;
@@ -88,25 +90,46 @@ function formatGPXExcursions($gpxFile)
 
     // Remplissage des intervalles nulles
     foreach ($intervallesNulles as $intervalle) {
-        $contientPremierPoint = $intervalle["min"] == 0;
-        $contientDernierPoint = $intervalle["max"] == count($points) - 1;
+        $indexPointDepart = $intervalle["min"];
+        $indexPointArrivee = $intervalle["max"];
+        $nbPoints = $indexPointArrivee - $indexPointDepart + 1;
 
-        if (!$contientPremierPoint && !$contientDernierPoint) {
+        $contientPremierPoint = $indexPointDepart == 0;
+        $contientDernierPoint = $indexPointArrivee == count($points) - 1;
+
+        if ($contientPremierPoint) {
+            // Intervalle contient le premier point
+            $pointSuivant = $points[$indexPointArrivee + 1];
+
+            // On copie l altitude du point suivant sur tous les points de l intervalle
+            for ($i = 0; $i < $nbPoints; $i++) {
+                $points[$i]["alt"] = $pointSuivant["alt"];
+            }
+        } else if ($contientDernierPoint) {
+            // Intervalle contient le dernier point
+            $pointPrecedent = $points[$indexPointDepart - 1];
+
+            // On copie l altitude du point precedent sur tous les points de l intervalle
+            for ($i = 0; $i < $nbPoints; $i++) {
+                $points[$indexPointArrivee - $i]["alt"] = $pointSuivant["alt"];
+            }
+        } else {
             // Seulement si les intervalles ne contiennent pas le premier ou dernier point
             // Car on ne peut pas tracer de droite avec un seul point
-            $pointDepart = $points[$intervalle["min"] - 1];
-            $pointArrivee = $points[$intervalle["max"] + 1];
-            $deltaAltitude = $pointArrivee["alt"] - $pointDepart["alt"]; // Negatif si descente
-            $deltaDistance = $pointArrivee["dist"] - $pointDepart["dist"]; // Forcement positif
+            $pointPrecedent = $points[$indexPointDepart - 1];
+            $pointSuivant = $points[$indexPointArrivee + 1];
+
+            $deltaAltitude = $pointSuivant["alt"] - $pointPrecedent["alt"]; // Negatif si descente
+            $deltaDistance = $pointSuivant["dist"] - $pointPrecedent["dist"]; // Forcement positif
+
             $pente = $deltaAltitude / $deltaDistance;            // Coeficient de proportionnalite de la distance dans l intervalle
 
-            $nbPoints = $intervalle["max"] - $intervalle["min"] + 1;
             for ($i = 0; $i < $nbPoints; $i++) {
                 // Calcul de l altitude de chaque point
-                $point = $points[$intervalle["min"] + $i];
-                $distance = $point["dist"] - $pointDepart["dist"];          // Distance du point par rapport au premier point
-                $altitude = $pointDepart["alt"] + $pente * $distance;       // Fonction affine
-                $points[$intervalle["min"] + $i]["alt"] = $altitude;
+                $point = $points[$indexPointDepart + $i];
+                $distance = $point["dist"] - $pointPrecedent["dist"];          // Distance du point par rapport au premier point
+                $altitude = $pointPrecedent["alt"] + $pente * $distance;       // Fonction affine
+                $points[$indexPointDepart + $i]["alt"] = $altitude;
             }
         }
     }
@@ -123,7 +146,7 @@ function formatGPXExcursions($gpxFile)
     return json_encode($points);
 }
 
-echo formatGPXExcursions("exemple.gpx");
+echo formatGPXExcursions("./gpx_exemples/marcatiecho_circuito.gpx");
 
 
 /* ------------------------------- PRIMITIVES ------------------------------- */
