25dedc43f1ba8ee7d8496c0c9d469cf4e8b2ce54
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-12-14 12:14:04 +0100

    Ajout d'un lien pour tester la page

--- src/app/screens/CarteScreen.tsx
+++ src/app/screens/CarteScreen.tsx
@@ -2,17 +2,18 @@ import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
-import { Screen, Text } from "app/components"
+import { Screen, Text, Button } from "app/components"
 import { spacing } from "app/theme"
 
 
 interface CarteScreenProps extends AppStackScreenProps<"Carte"> { }
 
-export const CarteScreen: FC<CarteScreenProps> = observer(function CarteScreen() {
+export const CarteScreen: FC<CarteScreenProps> = observer(function CarteScreen(props) {
 
   return (
     <Screen style={$root} preset="scroll">
       <Text text="Carte" size="xxl" />
+      <Button text="TEST nouveau signalement" onPress={() => props.navigation.navigate("Stack", { screen: "NouveauSignalement" })} />
     </Screen>
   )
 })
