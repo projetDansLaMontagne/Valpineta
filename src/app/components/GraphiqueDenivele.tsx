862db565e0aedc1b4497e439afaafdcebca12d35
Oier Cesat <76811979+oiercesat@users.noreply.github.com>
2023-12-19 16:26:57 +0100

    Ajout de la description des excursions (#8)
    * Création de la page Description
    
    * Modification des props pour detail excursion
    
    * changement du passage des variables entre Excursions -> DetailsExcursion -> Description
    
    * fin de l'implémentation de la page description
    
    * Affichage de la description sur la page details
    
    * Suppresion des sauts de lignes au début et des tabulations entre les paragraphes
    
    * correction erreur cannot read ... of undefined en initialisant les variables
    
    * réglages de beugs et amélioration du passage de paramètres
    
    * Ajout du nouveau JSON
    
    * Réglage de beugs sur les filtres
    
    * Meilleure gestion du passage d'excursion
    
    * Resolution d'erreur
    
    * Typage de certains paramètres
    
    * Vérification de props.route.params
    
    * Merge avec main
    
    * Correction du beug avec props
    
    * Description stable avec gestion d'erreur et amélioration du code
    
    * TextALign justify
    
    * Dernière correction de PR
    
    * changement variable description
    
    * Ajout du 2e return dans le else
    
    * Remise en forme de MapScreen
    
    ---------
    
    Co-authored-by: unknown <alexandre.pascal.ep@gmail.com>
    Co-authored-by: Nicolas-Delahaie <nico601.delahaie@gmail.com>

--- src/app/components/GraphiqueDenivele.tsx
+++ src/app/components/GraphiqueDenivele.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { StyleProp, View, ViewStyle,Dimensions } from "react-native"
+import React, { FC, useEffect, useState } from "react"
+import { StyleProp, View, ViewStyle, Dimensions, ActivityIndicator } from "react-native"
 import { observer } from "mobx-react-lite"
 import { colors, spacing } from "app/theme"
-import { LineChart } from 'react-native-chart-kit'
+import { LineChart } from "react-native-chart-kit"
 
 export interface GraphiqueDeniveleProps {
   /**
@@ -19,58 +19,75 @@ export interface GraphiqueDeniveleProps {
 /**
  * Describe your component here
  */
-export const GraphiqueDenivele = observer(function GraphiqueDenivele(props: GraphiqueDeniveleProps) {
-
-    //Lageur de l'écran
-    const { width } = Dimensions.get("window");
-
-    //Données du graphique
-    const data = props.data;
-
-    //Récupération des altitudes
-    const coordonnees = data.features[0].geometry.coordinates[0];
-    //On ne garde que les altitudes tous les 50 points
-    const listeALtitude = coordonnees
-      .map((item, index) => (index % 50 === 0 ? item[2] : null))
-      .filter((altitude) =>altitude !== null);
-
-    //Données du graphique avec labels et les données
-    const line = {
-      labels: ["0", "200", "400", "600", "800", "1000"],
-      datasets: [
-        {
-          data: listeALtitude,
-        },
-      ],
-    };
-
-    return (
-      <View>
-        <LineChart
-          data={line}
-          width={width - spacing.xl * 2}
-          height={200}
-          withInnerLines={false}
-          withOuterLines={false}
-          withShadow={false}
-          chartConfig={{
-            backgroundColor: colors.fond,
-            backgroundGradientFrom: colors.fond,
-            backgroundGradientTo: colors.fond,
-            color: () => colors.boutonAttenue,
-            labelColor: () => colors.bouton,
-            style: {
-              borderRadius: 16
-            },
-            propsForDots: {
-              r: "0",
-              strokeWidth: "0",
-              stroke: colors.bouton
-            }
-          }}
-          bezier
-        />
-      </View>
-    );
+export const GraphiqueDenivele = observer(function GraphiqueDenivele(
+  props: GraphiqueDeniveleProps,
+) {
+  //Chargement du graphique de dénivelé
+  const chrono = () => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
   }
-);
+
+  //Chargement du graphique de dénivelé
+  const [isLoading, setIsLoading] = useState(true)
+
+  //Lance le chrono pour le chargement du graphique de dénivelé
+  useEffect(() => {
+    chrono()
+  }, [])
+
+  //Lageur de l'écran
+  const { width } = Dimensions.get("window")
+
+  //Données du graphique
+  const data = props.data
+
+  //Récupération des altitudes
+  const coordonnees = data.features[0].geometry.coordinates[0]
+  //On ne garde que les altitudes tous les 50 points
+  const listeALtitude = coordonnees
+    .map((item, index) => (index % 50 === 0 ? item[2] : null))
+    .filter((altitude) => altitude !== null)
+
+  //Données du graphique avec labels et les données
+  const line = {
+    labels: ["0", "200", "400", "600", "800", "1000"],
+    datasets: [
+      {
+        data: listeALtitude,
+      },
+    ],
+  }
+
+  return isLoading ? (
+    <ActivityIndicator size="large" color={colors.bouton} />
+    ) : (
+    <View>
+      <LineChart
+        data={line}
+        width={width - spacing.xl * 2}
+        height={200}
+        withInnerLines={false}
+        withOuterLines={false}
+        withShadow={false}
+        chartConfig={{
+          backgroundColor: colors.fond,
+          backgroundGradientFrom: colors.fond,
+          backgroundGradientTo: colors.fond,
+          color: () => colors.boutonAttenue,
+          labelColor: () => colors.bouton,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "0",
+            strokeWidth: "0",
+            stroke: colors.bouton,
+          },
+        }}
+        bezier
+      />
+    </View>
+  )
+})
