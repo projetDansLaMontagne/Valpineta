fbc233d064b8a81868185c545089ac1fe4415d38
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2024-02-13 10:09:41 +0100

    Refactor SuiviExcursion (integration des actions consecutives directement dans le setEtat)

--- src/app/models/SuiviExcursion.ts
+++ src/app/models/SuiviExcursion.ts
@@ -2,6 +2,7 @@ import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree";
 import { withSetPropAction } from "./helpers/withSetPropAction";
 import { reaction } from "mobx";
 import * as Location from "expo-location";
+import { TPoint } from "app/navigators";
 
 const T_point_GPX = types.model({
   lat: types.number,
@@ -14,11 +15,11 @@ const T_point = types.model({
   lat: types.number,
   lon: types.number,
   alt: types.number,
+  dist: types.number,
 });
 
 type T_EtatExcursion = "nonDemarree" | "enCours" | "enPause" | "terminee";
 const BACKGROUND_LOCATION_TASK_NAME = "background-location-task"; // Static sinon boucle entre suiviExcursion.ts et app.tsx
-const BACKGROUND_POSITIONNING_ON_TRACK_TASK_NAME = "background-positionning-on-track-task"; // Static sinon boucle entre suiviExcursion.ts et app.tsx
 
 /**
  * Model description here for TypeScript hints.
@@ -26,113 +27,102 @@ const BACKGROUND_POSITIONNING_ON_TRACK_TASK_NAME = "background-positionning-on-t
 export const SuiviExcursionModel = types
   .model("SuiviExcursion")
   .props({
-    // types.frozen pour ne pas repeter les types.
     etat: types.optional(types.frozen(), "nonDemarree"),
     trackReel: types.optional(types.array(T_point_GPX), []),
     trackSuivi: types.optional(types.array(T_point), []),
-    pointCourant: types.optional(types.number, 0),
+    iPointCourant: types.optional(types.number, 0),
+    __debug__: types.optional(types.boolean, true),
   })
   .actions(withSetPropAction)
   .views(self => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions(self => {
-    function afterCreate() {
-      reaction(
-        () => self.etat,
-        etat => {
-          switch (etat) {
-            case "nonDemarree":
-              setTrackReel([]);
-              setTrackSuivi([]);
-              setPointCourant(0);
-              console.log("Reinitialisation infos de suivi");
-              break;
-            case "enCours":
-              startSuiviTrackReel();
-              break;
-            case "enPause":
-              stopSuiviTrackReel();
-              break;
-            case "terminee":
-              stopSuiviTrackReel();
-              // sauvegarde du track reel
-              // publication avis
-              break;
-          }
-        },
-      );
-    }
-
     /* -------------------------------------------------------------------------- */
     /*                                   PUBLIC                                   */
     /* -------------------------------------------------------------------------- */
+    type setEtatParams =
+      | {
+          newEtat: "enCours";
+          trackSuivi: Instance<typeof T_point>[];
+        }
+      | { newEtat: Exclude<T_EtatExcursion, "enCours"> };
     /**
      * Méthode pour changer l etat
      * @prerequis le nouvel etat doit etre possible
      * @param params parametres supplementaires telle que l excursion modele si on lance une nouvelle excursion
      */
-    function setEtat(newEtat: T_EtatExcursion, trackSuivi?: Instance<typeof T_point>[]) {
-      var changementPossible = false;
+    async function setEtat(props: setEtatParams): Promise<boolean> {
+      console.log("CHANGEMENT ETAT");
+      const { newEtat } = props;
 
-      // Matrice des changements d'état possibles notee sur papier
+      /* ---------------------------------- Debug --------------------------------- */
+      if (self.__debug__ && newEtat == "enCours") {
+        // En debug, on met un track personnalise pour pouvoir faire des tests
+        const track: TPoint[] = require("assets/tests_examples/excursions.json")[0].track;
+        props.trackSuivi = track as Instance<typeof T_point>[];
+      }
+
+      /* ------------------------------ Verifications ----------------------------- */
+      var preRequisOK = false;
+      // etats possibles
       switch (self.etat) {
         case "nonDemarree":
-          if (newEtat === "enCours") changementPossible = true;
+          if (newEtat === "enCours") preRequisOK = true;
           break;
 
         case "enCours":
-          if (newEtat === "enPause" || newEtat === "terminee") changementPossible = true;
+          if (newEtat === "enPause" || newEtat === "terminee") preRequisOK = true;
           break;
 
         case "enPause":
-          if (newEtat === "enCours" || newEtat === "terminee") changementPossible = true;
+          if (newEtat === "enCours" || newEtat === "terminee") preRequisOK = true;
           break;
 
         case "terminee":
-          if (newEtat === "nonDemarree") changementPossible = true;
-          break;
-
-        default:
-          console.log("[ERREUR] Etat excursion inconnu");
+          if (newEtat === "nonDemarree") preRequisOK = true;
           break;
       }
+      // params
+      if (newEtat === "enCours" && !props.trackSuivi) {
+        preRequisOK = false;
+      }
 
-      // Cas specifique : Verif params supplementaires
-      // if (newEtat === "enCours" && !trackSuivi) {
-      //   changementPossible = false;
-      // }
+      /* -------------------------- Actions consecutives -------------------------- */
+      if (preRequisOK) {
+        var aFonctionne: boolean;
+        switch (newEtat) {
+          case "nonDemarree":
+            setTrackReel([]);
+            setTrackSuivi([]);
+            setPointCourant(0);
+            console.log("Reinitialisation infos de suivi");
+            aFonctionne = true; //Tjrs vrai
+            break;
+          case "enCours":
+            setTrackSuivi(props.trackSuivi);
+            aFonctionne = await startBackgroundTask();
+            break;
+          case "enPause":
+            stopSuiviTrackReel();
+            aFonctionne = true;
+            break;
+          case "terminee":
+            stopSuiviTrackReel();
+            // sauvegarde du track reel
+            // publication avis
+            aFonctionne = true;
+            break;
+        }
 
-      // Attribution
-      if (changementPossible) {
-        self.etat = newEtat;
-
-        // if (newEtat === "enCours") {
-        //   setTrackSuivi(trackSuivi);
-        //   const track: T_excursion = require("assets/JSON/excursions.json")[0].track;
-        //   setTrackSuivi(track);
-        //   console.log(track.nom);
-        // }
+        if (aFonctionne === true) {
+          self.etat = newEtat;
+          return true;
+        } else {
+          console.warn("[SuiviExcursion] Erreur : Impossible de changer d etat");
+          return false;
+        }
       } else {
-        console.log("Changement d etat impossible : mauvaise utilisation de setEtat()");
-      }
-
-      switch (newEtat) {
-        case "nonDemarree":
-          setTrackReel([]);
-          setTrackSuivi([]);
-          setPointCourant(0);
-          console.log("Reinitialisation infos de suivi");
-          break;
-        case "enCours":
-          startSuiviTrackReel();
-          break;
-        case "enPause":
-          stopSuiviTrackReel();
-          break;
-        case "terminee":
-          stopSuiviTrackReel();
-          // sauvegarde du track reel
-          // publication avis
-          break;
+        console.warn("[SuiviExcursion] Erreur : Pre requis violés");
+        return false;
       }
     }
     /**
@@ -140,15 +130,19 @@ export const SuiviExcursionModel = types
      * @returns booleen indiquant si les permissions ont ete obtenues
      */
     async function requestPermissions(): Promise<boolean> {
-      const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
-      if (foregroundStatus === "granted") {
-        const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
-        if (backgroundStatus === "granted") {
-          console.log("[SuiviExucrsion] LOCC BACK OK");
-          return true;
+      try {
+        const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+        if (foregroundStatus === "granted") {
+          const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+          if (backgroundStatus === "granted") {
+            return true;
+          }
         }
+        return false;
+      } catch (e) {
+        console.warn("[requestPermissions]:", e);
+        return false;
       }
-      return false;
     }
     function ajoutPointTrackReel(point: Instance<typeof T_point_GPX>) {
       self.trackReel.push(point);
@@ -161,29 +155,31 @@ export const SuiviExcursionModel = types
     /**
      * Methode privee pour demarrer la tache de fond de suivi, en s assurant que les permissions sont ok
      */
-    async function startSuiviTrackReel() {
+    async function startBackgroundTask(): Promise<boolean> {
       const permissionsOK = await requestPermissions();
 
-      BACKGROUND_POSITIONNING_ON_TRACK_TASK_NAME;
       if (permissionsOK) {
         if (!(await Location.hasStartedLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME))) {
-          Location.startLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME, {
+          await Location.startLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME, {
             accuracy: Location.Accuracy.BestForNavigation,
-            // timeInterval: 1000,
+            // timeInterval: 5000,
             // distanceInterval: 5
             // showsBackgroundLocationIndicator: true,
             // deferredUpdatesDistance: 20,
-            foregroundService: {
-              notificationTitle: "Using your location",
-              notificationBody: "To turn off, go back to the app and switch something off.",
-              killServiceOnDestroy: true,
-            },
-          });
 
+            // foregroundService: {
+            //   notificationTitle: "Using your location",
+            //   notificationBody: "To turn off, go back to the app and switch something off.",
+            //   killServiceOnDestroy: true,
+            // },
+          });
           console.log("Suivi en arrière-plan démarré");
         } else {
           console.log("Suivi en arrière-plan déjà en cours");
         }
+        return true;
+      } else {
+        return false;
       }
     }
     /**
@@ -191,7 +187,7 @@ export const SuiviExcursionModel = types
      */
     async function stopSuiviTrackReel() {
       if (await Location.hasStartedLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME)) {
-        Location.stopLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME);
+        await Location.stopLocationUpdatesAsync(BACKGROUND_LOCATION_TASK_NAME);
       }
       console.log("Arrêt du suivi");
     }
@@ -211,10 +207,10 @@ export const SuiviExcursionModel = types
      * Setter privé pour le point courant
      */
     function setPointCourant(value: number) {
-      self.pointCourant = value;
+      self.iPointCourant = value;
     }
 
-    return { afterCreate, setEtat, requestPermissions, ajoutPointTrackReel };
+    return { setEtat, requestPermissions, ajoutPointTrackReel };
   }); // eslint-disable-line @typescript-eslint/no-unused-vars
 export interface SuiviExcursion extends Instance<typeof SuiviExcursionModel> {}
 export interface SuiviExcursionSnapshotOut extends SnapshotOut<typeof SuiviExcursionModel> {}
